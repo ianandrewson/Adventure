3a317e3d532d2ccd2c28fcbba4ab529777393db5
Ian Andrewson <ianandrewson@gmail.com>
2019-10-16 16:45:56 -0700

    added metadata skeleton

---
+++ Data/quest-data.js
@@ -0,0 +1,46 @@
+const quest1 = {
+    ID:
+    TITLE:
+    MAP:
+    IMAGE:
+    AUDIO:
+    ACTION:
+    DESCRIPTION:
+    CHOICES:
+        ID:
+        DESCRIPTION:
+        RESULT:
+        CONSEQUENCES:
+}
+
+const quest2 = {
+    ID:
+    TITLE:
+    MAP:
+    IMAGE:
+    AUDIO:
+    ACTION:
+    DESCRIPTION:
+    CHOICES:
+        ID:
+        DESCRIPTION:
+        RESULT:
+        CONSEQUENCES:
+}
+
+const quest3 = {
+    ID: 
+    TITLE: 
+    MAP: 
+    IMAGE: 
+    AUDIO: 
+    ACTION: 
+    DESCRIPTION: 
+    CHOICES: 
+        ID: 
+        DESCRIPTION: 
+        RESULT: 
+        CONSEQUENCES: 
+}
+
+export const quests = [quest1, quest2, quest3]
