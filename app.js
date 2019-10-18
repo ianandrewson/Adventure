1b4bc4999a5c19ed1d2c0cee3196a9ce594bcfd2
Ian Andrewson <ianandrewson@gmail.com>
2019-10-17 17:40:17 -0700

    implemented redirect to map page after creating user profile

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import { newUser } from './src/newUser.js';
 
 const submitUser = document.getElementById('submit-user');
 
-submitUser.addEventListener('submit', () => {
+submitUser.addEventListener('submit', (event) => {
+    event.preventDefault();
     const userData = new FormData(submitUser);
     newUser(userData);
     window.location = 'map';
