94f1c277b3e56edf420f5bec7a1e06cc7133b240
Ian Andrewson <ianandrewson@gmail.com>
2019-10-20 12:22:33 -0700

    expanded checkDeathOrWin test to test for win condition

--- test/checkDeathOrWin.test.js
+++ test/checkDeathOrWin.test.js
@@ -5,7 +5,7 @@ import { checkDeathOrWin } from '../src/checkDeathOrWin.js';
 
 const test = QUnit.test;
 
-test('Should return a new window location for results', function(assert) {
+test('Should return a new window location to results for loss', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const user = {
@@ -13,6 +13,32 @@ test('Should return a new window location for results', function(assert) {
         occupation: 'Water Reclaimation Specialist',
         ship: 'Scout',
         health: 0,
+        fuel: 60,
+        standing: 0,
+        credits: 0,
+        completed: {}
+    };
+    const expected = '../results/';
+
+
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = checkDeathOrWin(user);
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, expected);
+});
+
+test('Should return a new window location to results for win', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const user = {
+        name: 'Ian',
+        occupation: 'Water Reclaimation Specialist',
+        ship: 'Scout',
+        health: 20,
         fuel: 60,
         standing: 0,
         credits: 0,
