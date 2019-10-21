d8b8a0a56597743674dde04b05c945c1a8073f9f
Ian Andrewson <ianandrewson@gmail.com>
2019-10-20 17:51:03 -0700

    attempt to fix mysterious travis error 003. Why would a test be able to locally load a module and pass the test, but unable to remotely?

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 import './newUser.test.js';
-//import './generateQuestLink.test.js';
+import './generateQuestLink.test.js';
 import './renderChoice.test.js';
 import './renderQuestResult.test.js';
 import './updatePlayerStats.test.js';
